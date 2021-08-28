a89ab0bc6a1a4abf5931bda045895d36ff63a596
Vladlen Morozov <chapouis@yandex.ru>
2021-08-28 13:33:37 +0300

    Button with post screen with title from feedViewController added

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -7,14 +7,49 @@
 
 import UIKit
 
+struct Post {
+    var title: String
+}
+
 class FeedViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-    
+    let button = UIButton(frame: CGRect(x: 100, y: 300, width: 200, height: 50))
+        button.setTitle("See the Post!", for: .normal)
+        button.addTarget(self, action: #selector(tap), for: .touchUpInside)
+        button.backgroundColor = .darkGray
+        button.setTitleColor(.black, for: .normal)
+        
+        view.addSubview(button)
+        
     }
     
-
-
+    @objc func tap() {
+        let vc = PostViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    let alert = UIAlertController(title: "Error", message: "AZAZA", preferredStyle: .alert)
+    let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) {
+        _ in print("Cancel")
+    }
+    
+    var firstPost = Post(title: "FirstPost")
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "FiresPost" {
+        if let postName = segue.destination as? PostViewController {
+            postName.title = "FirstPost"
+        }
+    }
 }
+}
+
+
+
+
+
+    
+
---
+++ Navigation/PostViewController.swift
@@ -0,0 +1,26 @@
+//
+//  PostViewController.swift
+//  Navigation
+//
+//  Created by Владлен Морозов on 27.08.2021.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    var postName = "FirstPost"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .magenta
+        
+        self.title = postName
+        
+    }
+ 
+
+    
+}
+

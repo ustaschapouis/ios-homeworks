099d27551252df6560169e4ef03dc84b59a047f4
Vladlen Morozov <chapouis@yandex.ru>
2021-09-22 23:59:13 +0300

    Section with PhotoViewCell added, Strelka - temporary decision, gonna fix this

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -10,8 +10,6 @@ import UIKit
 class ProfileViewController: UIViewController {
     let cellID = "cellID"
     let tableView = UITableView(frame: .zero, style: .grouped)
-
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,19 +18,18 @@ class ProfileViewController: UIViewController {
         
         setupView()
         setupConstraints()
-    
     }
  
     func setupView () {
         view.addSubview(tableView)
+        view.backgroundColor = .systemGray5
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.register(PostTableViewCell.self, forCellReuseIdentifier: cellID)
         tableView.register(ProfileTableHeaderView.self, forHeaderFooterViewReuseIdentifier: "header")
+        tableView.register(PhotosTableViewCell.self, forCellReuseIdentifier: "Photos")
         tableView.delegate = self
         tableView.dataSource = self
         tableView.backgroundColor = .white
-        
-
     }
     
     func setupConstraints() {
@@ -43,37 +40,59 @@ class ProfileViewController: UIViewController {
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
         ])
     }
-    
 }
 
 extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 1
+        }
         return Posts.content.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Photos", for: indexPath) as! PhotosTableViewCell
+            cell.feedPhoto1.image = UIImage(named: Posts.photos[indexPath.row])
+            cell.feedPhoto2.image = UIImage(named: Posts.photos[indexPath.row + 1])
+            cell.feedPhoto3.image = UIImage(named: Posts.photos[indexPath.row + 2])
+            cell.feedPhoto4.image = UIImage(named: Posts.photos[indexPath.row + 3])
+            return cell
+        }
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID) as! PostTableViewCell
-        let postCell = Posts.content[indexPath.row]
+            let postCell = Posts.content[indexPath.row]
         cell.authorLabel.text = postCell.author
         cell.postImage.image = UIImage(named: postCell.image)
         cell.descriptionLabel.text = postCell.description
         cell.likesLabel.text = "Likes: \(postCell.likes)"
         cell.viewsLabel.text = "Views: \(postCell.views)"
-        
-        
-                
-        return cell
-    }
+            return cell
+
+        }
 
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let view = tableView.dequeueReusableHeaderFooterView(withIdentifier: "header") as! ProfileTableHeaderView
-        return view
         
+        if section == 0 {
+            let view = tableView.dequeueReusableHeaderFooterView(withIdentifier: "header") as! ProfileTableHeaderView
+            return view
+        } else {
+            return nil
+        }
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == 0 {
         return 220
+        } else {
+            return 0
+        }
     }
-    
-    
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
 }
+    
+    
+
